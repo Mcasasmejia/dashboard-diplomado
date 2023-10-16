18d63b7f8f7c659c6680ae11169087749c3393be
Michael Casas <89426867+Mcasasmejia@users.noreply.github.com>
2023-10-16 15:19:05 -0500

    Actualización Hoteles - Habitaciones

--- dashboard-diplomado/src/App.vue
+++ dashboard-diplomado/src/App.vue
@@ -1,122 +1,21 @@
 <script setup>
-
+import { createRouter, createWebHistory } from 'vue-router'
 import HeaderPage from './components/HeaderPage.vue';
-
+import SiderBar from './components/SiderBar.vue';
 </script>
 
 <template>
   <div>
-    <!-- start: Sidebar -->
-    <div class="fixed left-0 top-0 w-64 h-full bg-gray-900 p-4 z-50 sidebar-menu transition-transform">
-        <a href="#" class="flex items-center pb-4 border-b border-b-gray-800">
-            <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded object-cover">
-            <span class="text-lg font-bold text-white ml-3">Las Casas</span>
-        </a>
-        <ul class="mt-4">
-            <li class="mb-1 group active">
-                <a href="./"
-                    class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                    <i class="ri-home-2-line mr-3 text-lg"></i>
-                    <span class="text-sm">Dashboard</span>
-                </a>
-            </li>
-            <li class="mb-1 group">
-                <a href="./src/pages/hoteles.html"
-                    class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                    <i class="ri-hotel-line mr-3 text-lg"></i>
-                    <span class="text-sm">Hoteles</span>
-                </a>
-            </li>
-            <li class="mb-1 group">
-                <a
-                    class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle">
-                    <i class="ri-hotel-bed-line mr-3 text-lg"></i>
-                    <span class="text-sm">Habitaciones</span>
-                    <i class="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
-                </a>
-                <ul class="pl-7 mt-2 hidden group-[.selected]:block">
-                    <li class="mb-4">
-                        <a class="text-white text-sm flex items-center hover:text-gray-100 mt-4">Tipos</a>
-                        <ul style="list-style: none; padding-left: 20px;">
-                            <li style="margin-top: 10px;">
-                                <label>
-                                    <input type="checkbox" name="opcion1">
-                                    <span style="color: white; margin-left: 10px;">Estandar</span>
-                                </label>
-                            </li>
-                            <li style="margin-top: 10px;">
-                                <label>
-                                    <input type="checkbox" name="opcion2">
-                                    <span style="color: white; margin-left: 10px;">Junior</span>
-                                </label>
-                            </li>
-                            <li style="margin-top: 10px;">
-                                <label>
-                                    <input type="checkbox" name="opcion3">
-                                    <span style="color: white; margin-left: 10px;">Suite</span>
-                                </label>
-                            </li>
-                        </ul>
-                    </li>
 
-                    <li class="mb-4">
-                        <a class="text-white text-sm flex items-center hover:text-gray-100 mt-4">Acomodación</a>
-                        <ul style="list-style: none; padding-left: 20px;">
-                            <li style="margin-top: 10px;">
-                                <label>
-                                    <input type="checkbox" name="opcion1">
-                                    <span style="color: white; margin-left: 10px;">Sencilla</span>
-                                </label>
-                            </li>
-                            <li style="margin-top: 10px;">
-                                <label>
-                                    <input type="checkbox" name="opcion2">
-                                    <span style="color: white; margin-left: 10px;">Doble</span>
-                                </label>
-                            </li>
-                            <li style="margin-top: 10px;">
-                                <label>
-                                    <input type="checkbox" name="opcion3">
-                                    <span style="color: white; margin-left: 10px;">Triple</span>
-                                </label>
-                            </li>
-                            <li style="margin-top: 10px;">
-                                <label>
-                                    <input type="checkbox" name="opcion3">
-                                    <span style="color: white; margin-left: 10px;">Cuadruple</span>
-                                </label>
-                            </li>
-                        </ul>
-                    </li>
-                    <div class="flex items-center justify-center">
-                        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                            <a href="src/pages/habitaciones.html">Buscar</a>
-                        </button>
-                      </div>
-                      
-                </ul>
-            </li>
-            <li class="mb-1 group">
-                <a href="#" class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle">
-                    <i class="ri-file-chart-line mr-3 text-lg"></i>
-                    <span class="text-sm">Informes</span>
-                    <i class="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
-                </a>
-                <ul class="pl-7 mt-2 hidden group-[.selected]:block">
-                    <li class="mb-4">
-                        <a href="src/pages/informes_hab.html" class="text-gray-300 text-sm flex items-center hover:text-gray-100 before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">Gráficos Habitaciones</a>
-                    </li> 
-                    <li class="mb-4">
-                        <a href="src/pages/informes_acom.html" class="text-gray-300 text-sm flex items-center hover:text-gray-100 before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">Gráficos Acomodaciones</a>
-                    </li> 
-                </ul>
-            </li>
-        </ul>
-    </div>
-    <div class="fixed top-0 left-0 w-full h-full bg-black/50 z-40 md:hidden sidebar-overlay"></div>
-    <!-- end: Sidebar -->
+    <SiderBar/>
     
+    <main class="w-full md:w-[calc(100%-256px)] md:ml-64 bg-gray-50 min-h-screen transition-all main">
     <HeaderPage/>
+    <router-view></router-view>
+    </main>
+
+    
+
 
     
 
--- dashboard-diplomado/src/components/HeaderPage.vue
+++ dashboard-diplomado/src/components/HeaderPage.vue
@@ -1,6 +1,5 @@
 <template>
     <!-- start: Main -->
-    <main class="w-full md:w-[calc(100%-256px)] md:ml-64 bg-gray-50 min-h-screen transition-all main">
         <div class="py-2 px-6 bg-white flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30">
             <button type="button" class="text-lg text-gray-600 sidebar-toggle">
                 <i class="ri-menu-line"></i>
@@ -32,14 +31,6 @@
                 </li>
             </ul>
         </div>
-
-        <div>
-
-        </div>
-
-
-
-    </main>
     <!-- end: Main -->
 
 </template>
