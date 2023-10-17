7e5994640e9e44371dbf7bfc2c4a1139fadd2b5c
Michael Casas <89426867+Mcasasmejia@users.noreply.github.com>
2023-10-17 15:15:19 -0500

    Modales Hoteles

--- dashboard-diplomado/src/components/EditarHotel.vue
+++ dashboard-diplomado/src/components/EditarHotel.vue
@@ -13,15 +13,12 @@ function showModal() {
 </script>
 <template>
 <div>
-    <button @click="showModal" type="button" class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Crear Hotel
-    </button>
     <Modal :size="size" v-if="isShowModal" @close="closeModal">
-      <template #header>
-        <div class="flex items-center text-lg font-semibold text-gray-900">
-          Crear Hotel
-        </div>
-      </template>
+        <template #header>
+            <div class="flex items-center text-lg font-semibold text-gray-900">
+                Crear Hotel
+            </div>
+        </template>
       <template #body>
       <form>
         <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
